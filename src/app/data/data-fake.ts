7e440e727b7791288b13b11bd8f1cd0e0957fefe
gabrielmio <gabrielmio16@gmail.com>
2024-02-06 10:40:31 -0300

    Add content component css

--- src/app/data/data-fake.ts
+++ src/app/data/data-fake.ts
@@ -9,7 +9,7 @@ export const dataFake: Data[] = [
   {
     id: 1,
     title: 'Lorem ipsum dolor sit amet',
-    description: 'Cras quis pulvinar nulla. Donec aliquam, diam a sagittis suscipit, arcu libero lobortis orci, imperdiet consequat ligula massa id est.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in sodales nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas vehicula lectus nec mollis gravida. Duis a bibendum velit. Curabitur dapibus, turpis et sodales lobortis, lectus libero pharetra purus, iaculis pretium est velit ac purus. Pellentesque tincidunt pulvinar augue non aliquet. Maecenas dapibus ac elit ut lobortis. ',
     cover: 'https://purr.objects-us-east-1.dream.io/i/cat3.jpg'
   },
 ];
