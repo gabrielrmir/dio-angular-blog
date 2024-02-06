cad0498d3f69fbc0f378aacb8c9f9009ac41978d
gabrielmio <gabrielmio16@gmail.com>
2024-02-06 10:59:02 -0300

    Add data-fake factory

--- src/app/data/data-fake.ts
+++ src/app/data/data-fake.ts
@@ -5,12 +5,18 @@ interface Data {
   cover: string
 }
 
-export const dataFake: Data[] = [
-  {
-    id: 1,
+function generateData(id: number): Data {
+  return {
+    id,
     title: 'Lorem ipsum dolor sit amet',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in sodales nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas vehicula lectus nec mollis gravida. Duis a bibendum velit. Curabitur dapibus, turpis et sodales lobortis, lectus libero pharetra purus, iaculis pretium est velit ac purus. Pellentesque tincidunt pulvinar augue non aliquet. Maecenas dapibus ac elit ut lobortis. ',
     cover: 'https://purr.objects-us-east-1.dream.io/i/cat3.jpg'
-  },
-];
+  }
+}
 
+const dataFake: Data[] = [];
+for (let i = 1; i <= 19; i++) {
+  dataFake.push(generateData(i));
+}
+
+export {dataFake};
