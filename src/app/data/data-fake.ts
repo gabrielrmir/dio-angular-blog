3584679d1c7f266e39f42cabed490fd905c61d5d
gabrielmio <gabrielmio16@gmail.com>
2024-02-06 10:16:04 -0300

    Load content from data

---
+++ src/app/data/data-fake.ts
@@ -0,0 +1,16 @@
+interface Data {
+  id: number,
+  title: string,
+  description: string,
+  cover: string
+}
+
+export const dataFake: Data[] = [
+  {
+    id: 1,
+    title: 'Lorem ipsum dolor sit amet',
+    description: 'Cras quis pulvinar nulla. Donec aliquam, diam a sagittis suscipit, arcu libero lobortis orci, imperdiet consequat ligula massa id est.',
+    cover: 'https://purr.objects-us-east-1.dream.io/i/cat3.jpg'
+  },
+];
+
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { dataFake } from '../../data/data-fake';
 
 @Component({
   selector: 'app-content',
@@ -8,8 +9,29 @@ import { RouterLink } from '@angular/router';
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
   @Input() cover: string = '';
   @Input() title: string = '';
   @Input() description: string = '';
+
+  private id: number = 0;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(
+      params => this.id = Number(params.get('id'))
+    )
+
+    this.initializeComponents(this.id);
+  }
+
+  initializeComponents(id: number) {
+    const result = dataFake.filter(data => data.id === id)[0];
+
+    this.cover = result.cover;
+    this.title = result.title;
+    this.description = result.description;
+  }
+
 }
