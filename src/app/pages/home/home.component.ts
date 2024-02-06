922342f497b8b7359a3f4a40e1e3be2d0dfb3721
gabrielmio <gabrielmio16@gmail.com>
2024-02-06 12:07:19 -0300

    Add home cards dynamically

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,13 +4,14 @@ import { BigCardComponent } from '../../components/big-card/big-card.component';
 import { SmallCardComponent } from '../../components/small-card/small-card.component';
 import { CardsContainerComponent } from "../../components/cards-container/cards-container.component";
 import { CardData, dataFake } from '../../data/data-fake';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent, CardsContainerComponent]
+    imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent, CardsContainerComponent, CommonModule]
 })
 export class HomeComponent {
   cards: CardData[] = dataFake;
